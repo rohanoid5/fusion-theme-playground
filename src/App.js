e7fceaad5004c8c58ed7601a8c9a74edbb569a36
Rohan Dey <rohanoid5@gmail.com>
2019-12-10 15:34:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Material-UI stuff
+import { createMuiTheme, useTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+// TODO: Create a separate file for this
+import Blue from '@material-ui/core/colors/blue';
+import Green from '@material-ui/core/colors/green';
+import Container from '@material-ui/core/Container';
+
 function App() {
+  const [theme, setTheme] = useState({
+    palette: {
+      type: 'dark',
+      primary: Blue,
+      secondary: Green
+    },
+    status: {
+      danger: 'orange'
+    }
+  });
+  const toggleDarkTheme = () => {
+    let newPaletteType = theme.palette.type === 'light' ? 'dark' : 'light';
+    setTheme({
+      palette: {
+        type: newPaletteType,
+        primary: Blue,
+        secondary: Green
+      },
+      status: {
+        danger: 'orange'
+      }
+    });
+  };
+  const muiThemeApp = createMuiTheme(theme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={muiThemeApp}>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
